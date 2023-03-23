9192bd7469e32e75c28edf090fc035b91ddccf64
teerawat-cc <terawat.cc@gmail.com>
2023-03-23 15:47:32 +0700

    branch wind

---
+++ models/system.py
@@ -0,0 +1,26 @@
+class System:
+    def __init__(self):
+        self.__costomers = []
+        self.__admins = []
+        self.__categories = []
+        self.__products = []
+        self.__orders = []
+        self.__codes = []
+    
+    def function_costomers():
+        pass
+
+    def function_admins():
+        pass
+
+    def function_categories():
+        pass
+
+    def function_products():
+        pass
+
+    def function_orders():
+        pass
+
+    def function_codes(): #discount
+        pass
