4c73217bebc638a0cf7c836132202a6bd6ff9d89
ten14301 <ten077710@gmail.com>
2023-03-23 15:18:01 +0700

    update

--- models/main.py
+++ models/main.py
@@ -1,10 +1,13 @@
+
 class User:
-    def __init__(self,id:str,email: str,firstname:str,lastname:str,password:str):
+    def __init__(self,id:str,email: str,firstname:str,lastname:str,password:str,cart: str):
         self.id = id
         self.email = email
         self.firstname = firstname
         self.lastname = lastname
         self.password = password
+
+
 class Customer(User):
     def __init__(self,id:str,email:str,firstname:str,lastname:str,password:str,addresses:list):
         super().__init__(id,email, firstname, lastname, password)
@@ -12,8 +15,8 @@ class Customer(User):
         self.cart = []
     def add_address(self,address):
         self.addresses.append(address)
-    def add_cart(self,cart):
-        pass
+    def add_cart(self,carts):
+        self.cart.append(carts)
 
 
 class Admin(User):
@@ -29,12 +32,19 @@ class Address:
         self.phone = phone
         self.address = address
         self.zip_code = zip_code
+
 class Cart:
-    def __init__(self):
+    def __init__(self,id:str,name:str,price:float,description:str,image:str,last_updated:str):
         pass
-class Cartitem:
-    def __init__(self):
-        pass
+
+class CartItem():
+    def __init__(self,product,quantity:int):
+        self.project = product
+        self.quantity = quantity
+class Product():
+       pass
+
+product1 = Product('1','keyboard-it','300.00','testtttttt','/images/product','20-06-1987')
 
 
 
