c1178c44d65f7e2abd2d5a8d82afa2e09a6e0e02
teerawat-cc <terawat.cc@gmail.com>
2023-03-28 16:01:43 +0700

    update method System

--- models/System.py
+++ models/System.py
@@ -6,18 +6,30 @@ from typing import List
 # from Product        import Product
 # from Order          import Order
 from CodeDiscount   import CodeDiscount
-
+@dataclass
 class Customer:
-    pass
+    def create_customer(self, email, passward, firstname, lastname):
+        # do something with email, passward, firstname, lastname
+        return "Create customer completed"
+    def check_credential(self, email, passward):
+        # do something with email, passward
+        return "True or False"
+    
 class Admin:
     pass
+
 class Product:
     pass
+
 class Category:
     pass
-class Order:
-    pass
 
+class Order:
+    class order_list:
+        pass # have order in it!
+    def is_history_of_(self, email):
+        return "True or False"
+    
 @dataclass
 class System:
     customers: List[Customer]
@@ -26,3 +38,22 @@ class System:
     products: List[Product]
     orders: List[Order]
     codes: List[CodeDiscount]
+
+    def sign_up(self, email, passward, firstname, lastname):
+        status = Customer.create_customer(email, passward, firstname, lastname)
+        return status
+    
+    def sign_in(self, email, passward):
+        status = Customer.check_credential(email, passward)
+        if status == True :
+            return "Login completed"
+        else:
+            raise # return "Wrong email or password"
+            
+    def get_order_history(self, email): ##!
+        order_history_list = []
+        for order in Order.order_list:
+            status = order.is_history_of_(email)
+            if status: order_history_list.append(order)
+        return order_history_list
+    
