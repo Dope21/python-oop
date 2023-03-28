b0c837ece5890d6e0c7b4ef9ab218275fdfb3a04
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-03-28 12:19:00 +0700

    import reall user and order

--- models/System.py
+++ models/System.py
@@ -1,21 +1,13 @@
 from dataclasses import dataclass
 from typing import List
-# from Customer       import Customer
-# from Admin          import Admin
-# from Category       import Category
-# from Product        import Product
-# from Order          import Order
-from CodeDiscount   import CodeDiscount
 
-class Customer:
-    pass
-class Admin:
-    pass
+from User import Customer, Admin
+from Order import Order
+from CodeDiscount import CodeDiscount
+
 class Product:
     pass
 class Category:
-    pass
-class Order:
     pass
 
 @dataclass
