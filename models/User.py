a8b549f37246cad82e5590cac328c5a75327dbd2
ten14301 <49758110+ten14301@users.noreply.github.com>
2023-03-29 21:54:52 +0700

    Update User.py

--- models/User.py
+++ models/User.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import List
 from system import system
-from Address import Address
+from Address import Address,SetAddress
 
 @dataclass
 class User:
