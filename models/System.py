1a621f68f2a6385d567e272675c0a13e196ac308
teerawat-cc <terawat.cc@gmail.com>
2023-03-29 17:45:55 +0700

    update System

--- models/System.py
+++ models/System.py
@@ -3,14 +3,13 @@ from typing import List
 # from Customer       import Customer
 # from Admin          import Admin
 # from Category       import Category
+# from Subcategories import Subcategories
 # from Product        import Product
 # from Order          import Order
 from CodeDiscount   import CodeDiscount
+
 @dataclass
 class Customer:
-    def create_customer(self, email, passward, firstname, lastname):
-        # do something with email, passward, firstname, lastname
-        return "Create customer completed"
     def check_credential(self, email, passward):
         # do something with email, passward
         return "True or False"
@@ -24,23 +23,36 @@ class Product:
 class Category:
     pass
 
+class Subcategories:
+    pass
+
 class Order:
     class order_list:
         pass # have order in it!
     def is_history_of_(self, email):
-        return "True or False"
-    
+        return "True or False"    
+
 @dataclass
 class System:
     customers: List[Customer]
     admins: List[Admin]
     categories: List[Category]
+    subcategories: List[Subcategories]
     products: List[Product]
     orders: List[Order]
     codes: List[CodeDiscount]
 
+    # @staticmethod function that not need self
+
+    def create_customer(self, email, passward, firstname, lastname):
+        customer = Customer(email, passward, firstname, lastname)
+        status = self.customers.append(customer)
+        return status
+
     def sign_up(self, email, passward, firstname, lastname):
-        status = Customer.create_customer(email, passward, firstname, lastname)
+        system = System()
+        customer = system.create_customer(email, passward, firstname, lastname)
+        status = self.customers.append(customer)
         return status
     
     def sign_in(self, email, passward):
@@ -48,7 +60,7 @@ class System:
         if status == True :
             return "Login completed"
         else:
-            raise # return "Wrong email or password"
+            return "Wrong email or password"
             
     def get_order_history(self, email): ##!
         order_history_list = []
