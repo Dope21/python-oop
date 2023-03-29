e5cba4013b182e25edce2fdce78fbc8110de3576
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-03-29 21:32:07 +0700

    sign-in, sign-up complete

--- models/System.py
+++ models/System.py
@@ -3,48 +3,35 @@ from typing import List
 
 from .User import Customer, Admin
 from .Order import Order
+from .Product import Product
+from .Category import Category, SubCategory
 from .CodeDiscount import CodeDiscount
-
-class Order:
-    class order_list:
-        pass # have order in it!
-    def is_history_of_(self, email):
-        return "True or False"    
 
 @dataclass
 class System:
     customers: List[Customer]
     admins: List[Admin]
     categories: List[Category]
-    subcategories: List[Subcategories]
+    subcategories: List[SubCategory]
     products: List[Product]
     orders: List[Order]
     codes: List[CodeDiscount]
 
-    # @staticmethod function that not need self
-
+    # CUSTOMER MANAGEMENT
     def create_customer(self, email, passward, firstname, lastname):
         customer = Customer(email, passward, firstname, lastname)
         status = self.customers.append(customer)
         return status
 
-    def sign_up(self, email, passward, firstname, lastname):
-        system = System()
-        customer = system.create_customer(email, passward, firstname, lastname)
-        status = self.customers.append(customer)
-        return status
-    
-    def sign_in(self, email, passward):
-        status = Customer.check_credential(email, passward)
-        if status == True :
-            return "Login completed"
-        else:
-            return "Wrong email or password"
-            
-    def get_order_history(self, email): ##!
+    def sign_in(self, email, password):
+        for customer in self.customers:
+            if customer.check_credential(email, password):
+                return True
+        return False 
+         
+    def get_order_history(self, email): 
         order_history_list = []
-        for order in Order.order_list:
-            status = order.is_history_of_(email)
-            if status: order_history_list.append(order)
+        for order in self.orders:
+            if order.is_history_of(email):
+                order_history_list.append(order) 
         return order_history_list
-    
