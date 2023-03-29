3878fdef920a353889bfdf66d6db84a11118172c
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-03-29 22:36:40 +0700

    set default_dactory

--- models/System.py
+++ models/System.py
@@ -1,25 +1,26 @@
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, field
+from typing import List, Optional
 
 from .User import Customer, Admin
+from .Address import Address, SetAddress
 from .Order import Order
-from .Product import Product
-from .Category import Category, SubCategory
+from .Category import Category
 from .CodeDiscount import CodeDiscount
 
 @dataclass
 class System:
-    customers: List[Customer]
-    admins: List[Admin]
-    categories: List[Category]
-    orders: List[Order]
-    codes: List[CodeDiscount]
+    customers: List[Customer] = field(default_factory=list) 
+    admins: List[Admin] = field(default_factory=list)
+    categories: List[Category] = field(default_factory=list)
+    orders: List[Order] = field(default_factory=list)
+    codes: List[CodeDiscount] = field(default_factory=list)
 
     # CUSTOMER MANAGEMENT
-    def create_customer(self, email, passward, firstname, lastname):
-        customer = Customer(email, passward, firstname, lastname)
-        status = self.customers.append(customer)
-        return status
+    def create_customer(self, email, password, firstname, lastname):
+        address = [Address(firstname=firstname, lastname=lastname, set=SetAddress.MAIN)]
+        customer = Customer(id= "someid",email=email, password=password, firstname=firstname, lastname=lastname, address=address)
+        self.customers.append(customer)
+        return customer
 
     def sign_in(self, email, password):
         for customer in self.customers:
