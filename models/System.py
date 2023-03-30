9b01b6c51d4b1c4c5bc38eec17374d164037a8b3
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-03-30 14:04:24 +0700

    add_customer method to create_customer

--- models/System.py
+++ models/System.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import List
 
 from .User import Customer, Admin
 from .Address import Address, SetAddress
@@ -16,15 +16,23 @@ class System:
     codes: List[CodeDiscount] = field(default_factory=list)
 
     # CUSTOMER MANAGEMENT
-    def create_customer(self, email, password, firstname, lastname):
+    def create_customer(self, email, password, firstname, lastname, cart):
         address = [Address(firstname=firstname, lastname=lastname, set=SetAddress.MAIN)]
-        customer = Customer(id= "someid",email=email, password=password, firstname=firstname, lastname=lastname, address=address)
-        self.customers.append(customer)
+        customer = Customer(firstname=firstname,lastname=lastname,password=password,email=email, addresses=address, cart=cart)
+        self.add_customer(customer=customer)
         return customer
+    
+    def check_duplicate_email(self, email):
+        for customer in self.customers:
+           if customer.email == email:
+               return True 
+        return False
 
     def sign_in(self, email, password):
         for customer in self.customers:
-            if customer.check_credential(email, password):
+            if not customer.check_credential(email, password):
+                continue
+            else: 
                 return True
         return False 
          
@@ -34,3 +42,10 @@ class System:
             if order.is_history_of(email):
                 order_history_list.append(order) 
         return order_history_list
+    
+    def add_customer(self, customer):
+        if isinstance(customer, Customer):
+            self.customers.append(customer)
+            return True
+        else:
+            return False 
