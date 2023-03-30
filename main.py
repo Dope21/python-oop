87db2e5238586bee6df1aa2476da79f21a80bf61
teerawat-cc <terawat.cc@gmail.com>
2023-03-31 00:02:10 +0700

    update main branch to wind

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-from controllers import test_system
+from controllers import api_system, test_system
 
 # testing router
 app.include_router(test_system.router)
+app.include_router(api_system.router)
