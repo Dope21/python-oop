233c70dab2333fa7b4e07ca32a03a08ce322b6d7
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-02 14:35:19 +0700

    implemented basic add method

--- models/System.py
+++ models/System.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass, field
 from typing import List
-
 from .User import Customer, Admin
-from .Address import Address, SetAddress
 from .Order import Order
 from .Category import Category
 from .CodeDiscount import CodeDiscount
@@ -15,43 +13,43 @@ class System:
     orders: List[Order] = field(default_factory=list)
     codes: List[CodeDiscount] = field(default_factory=list)
 
-    # CUSTOMER MANAGEMENT
-    def create_customer(self, email, password, firstname, lastname, cart):
-        address = [Address(firstname=firstname, lastname=lastname, set=SetAddress.MAIN)]
-        customer = Customer(firstname=firstname,lastname=lastname,password=password,email=email, addresses=address, cart=cart)
-        self.add_customer(customer=customer)
-        return customer
-    
-    def check_duplicate_email(self, email):
-        for customer in self.customers:
-           if customer.email == email:
-               return True 
-        return False
-
     def sign_in(self, email, password):
         for customer in self.customers:
-            if customer.check_credential(email, password):
+            if customer.email == email and customer.password == password:
                 return True
         return False
 
-    def get_order_history(self, email): 
-        order_history_list = []
-        for order in self.orders:
-            if order.is_history_of(email):
-                order_history_list.append(order) 
-        return order_history_list
-    
     def add_customer(self, customer):
         if isinstance(customer, Customer):
             self.customers.append(customer)
             return True
         else:
+            return False
+
+    def add_admin(self, admin):
+        if isinstance(admin, Admin):
+            self.customers.append(admin)
+            return True
+        else:
             return False 
-        
-    def update_customer(self, customer):
-        for in_list in self.customers:
-            if in_list.email == customer.email:
-                self.customers.remove(in_list)
-                self.add_customer(customer)
-                return True
-        return False 
+     
+    def add_category(self, category):
+        if isinstance(category, Category):
+            self.customers.append(category)
+            return True
+        else:
+            return False 
+
+    def add_order(self, order):
+        if isinstance(order, Order):
+            self.customers.append(order)
+            return True
+        else:
+            return False 
+  
+    def add_code_discount(self, code_discount):
+        if isinstance(code_discount, CodeDiscount):
+            self.customers.append(code_discount)
+            return True
+        else:
+            return False
