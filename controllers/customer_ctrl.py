9179e46382a8fbd3099b7230a3a82258d494e131
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-02 16:18:31 +0700

    create customer controller

---
+++ controllers/customer_ctrl.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter()
+
+from init_system import system
+from schemas.customer_shcema import SignInInfo, CartItem
+
+@router.get("/get_all")
+async def get_all_customer():
+  return { "data": system.customers }
+
+@router.post("/sign_in")
+async def customer_login(signInInfo: SignInInfo):
+  customer = system.sign_in(signInInfo.email, signInInfo.password)
+  if not customer:
+   raise HTTPException(status_code=400, detail="Wrong email or password")
+
+  return { "message": "Sign in successful", "data": customer }
+
+@router.post("/add_cart")
+async def add_cart_item(email: str, cart_item: CartItem):
+  customer = system.find_customer_by_email(email)
+  if not customer:
+    raise HTTPException(status_code=400, detail="Customer not found")
+
+  product = system.find_product_by_id(cart_item.product_id)
+  if not product:
+    raise HTTPException(status_code=400, detail="Product not found")
+
+  customer.add_cart_item(product, cart_item.qty)
+  return { "message": "successs" }
+
+  
