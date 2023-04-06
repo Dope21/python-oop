6b4c7547e4594ecdd8656186c272ca22830c3db5
teerawat-cc <terawat.cc@gmail.com>
2023-04-06 22:22:11 +0700

    update main

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# from controllers import customer_ctrl
-# app.include_router(customer_ctrl.router)
+from controllers import customer_ctrl
+app.include_router(customer_ctrl.router)
 
-# from controllers import product
-# app.include_router(product.router)
+from controllers import product
+app.include_router(product.router)
 
 from controllers import discount_ctrl
 app.include_router(discount_ctrl.router)
