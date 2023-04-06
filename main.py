35b8d581389f9d97d66b380172397911ea2429e4
teerawat-cc <terawat.cc@gmail.com>
2023-04-06 22:13:08 +0700

    overwrite main branch wind

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-from controllers import api_system, test_system
+# from controllers import customer_ctrl
+# app.include_router(customer_ctrl.router)
 
-# testing router
-app.include_router(test_system.router)
-=======
-from controllers import customer_ctrl
+# from controllers import product
+# app.include_router(product.router)
 
-app.include_router(customer_ctrl.router)
->>>>>>> 751e626684844737a1ffd6f4d8dd5e03e9c6758d
-=======
-from controllers import product
-
-app.include_router(product.router)
->>>>>>> 8a8dfbb28c0bc4a3768c88f9e03e9000537f0a67
+from controllers import discount_ctrl
+app.include_router(discount_ctrl.router)
