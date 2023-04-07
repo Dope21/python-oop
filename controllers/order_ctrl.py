bf91e1531d388e707bb63e2c437d4b5c8d608d8f
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-07 15:53:27 +0700

    create checkout route

--- controllers/order_ctrl.py
+++ controllers/order_ctrl.py
@@ -1,15 +1,45 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-router = APIRouter()
+router = APIRouter(prefix="/order")
 
 from init_system import system
-from schemas.order_shcema import ItemList, Item, Payment, Shippinp
+from models.Order import Order, OrderItem, OrderStatus
+from models.Shipping import Shipping
+from schemas.order_shcema import CheckoutSchema
 
-@router.post("/order/checkout")
-async def checkout(item_list: ItemList, payment: Payment, shipping: Shippinp):
-  pass
+@router.post("/checkout")
+async def checkout(checkout_info: CheckoutSchema):
 
-@router.post("order/buynow")
-async def buynow(item: Item, payment: Payment, shipping: Shippinp):
-  pass
+  email = checkout_info.email
+  pay_info = checkout_info.pay_info
+  ship_info = checkout_info.ship_info
+  discount = checkout_info.discount
+
+  customer = system.get_customer_by_email(email)
+  if not customer:
+    raise HTTPException(status_code=400, detail=f"{email} is not exist")
+  
+  if not customer.cart:
+    raise HTTPException(status_code=400, detail="No item in the cart")
+
+  order_item_list = []
+  for item in customer.cart.cart_items:
+    order_item = OrderItem(item.product, item.qty, float("{:.2f}".format(item.product.price * discount)))
+    order_item_list.append(order_item)
+  
+  payment = system.create_payment(pay_info.method, pay_info.name_on_card) 
+  shipping = Shipping(ship_info.firstname, ship_info.lastname, ship_info.address, ship_info.phone, ship_info.zip_code)
+
+  order = Order(order_item_list, OrderStatus.OPEN, shipping, payment, email)
+  system.add_order(order)
+
+  if order.process_payment(pay_info):
+    return { "detail": "successful payment" }
+  else:
+    raise HTTPException(status_code=400, detail="Payment denied. Please try again later.")
+  
+
+# @router.post("/buynow")
+# async def buynow(product_id: str, payment: PaymentSchema, shipping: ShippinpSchma):
+  # pass
 
