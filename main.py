6061490c9ce30d374e73fd2616f14530c482eacb
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-07 15:56:10 +0700

    add order router

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 from controllers import customer_ctrl
+from controllers import order_ctrl
 
 app.include_router(customer_ctrl.router)
+app.include_router(order_ctrl.router)
