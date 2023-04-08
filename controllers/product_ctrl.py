a9a66be1c6e0c684c1d63e1d270afb8cf3e4599a
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-08 14:31:01 +0700

    arrange line

--- controllers/product_ctrl.py
+++ controllers/product_ctrl.py
@@ -1,34 +1,35 @@
 from fastapi import APIRouter
 
 router = APIRouter()
-from models.System import System
 from models.Product import *
 from models.Category import *
 from init_system import *
 
 @router.get("/all_catagory")
 async def admin():
-    res = system.list_category()
-    return res
+    return system.categories
 
 @router.get("/all_catagory_name")
 async def admin():
-    res = system.list_category_name()
-    return res
+    return system.list_category_name()
+
 @router.get("/catagory_by_name")
 async def admin():
     res = system.find_category_by_name("cateTest1")
     return res
+
 @router.get("/all_product_in_catagory_by_name")
 async def admin():
-    res = system.all_product_in_catagory("cateTest1")
-    return res
+    category = system.find_category_by_name(name)
+    name_list = category.get_all_product_name()
+    return name_list
+
 @router.get("/product_in_catagory_by_id")
 async def admin():
-    res = system.product_in_catagory_by_id("cateTest1","001")
+    res = system.find_product_by_id()
     return res
+
 @router.get("/product_in_catagory_by_name")
 async def admin():
     res = system.product_in_catagory_by_name("cateTest1","K2 SSR")
     return res
-
--- main.py
+++ main.py
@@ -5,9 +5,7 @@ app = FastAPI()
 from controllers import customer_ctrl
 from controllers import order_ctrl
 from controllers import product_ctrl
-from controllers import discount_ctrl
 
 app.include_router(customer_ctrl.router)
 app.include_router(order_ctrl.router)
 app.include_router(product_ctrl.router)
-app.include_router(discount_ctrl.router)
