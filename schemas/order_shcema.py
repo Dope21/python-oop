2b1be519e95e90887e5496b367b82ab6a1eba04c
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-08 14:11:47 +0700

    add CodeSchema

--- schemas/order_shcema.py
+++ schemas/order_shcema.py
@@ -1,10 +1,5 @@
 from pydantic import BaseModel
-<<<<<<< HEAD
 from typing import Optional
-=======
-from typing import List
-from models.Product import Product
->>>>>>> 1551b5e4aa630be81bbc6241c8fdfbba8b0e648d
 
 class Shippinp(BaseModel):
   firstname: str
@@ -24,15 +19,11 @@ class Payment(BaseModel):
   code: str
   method: str
 
-<<<<<<< HEAD
 class CheckoutSchema(BaseModel):
   email: str
   pay_info: Payment
   ship_info: Shippinp
   discount: Optional[float] = 1
-=======
 
-
->>>>>>> 1551b5e4aa630be81bbc6241c8fdfbba8b0e648d
-
-  
+class CodeSchema(BaseModel):
+  code: str
