283506bf0f031272fcda35f3cf718cc8aee8cc1b
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-18 21:01:58 +0700

    remove data class

--- models/Order.py
+++ models/Order.py
@@ -1,36 +1,65 @@
-from dataclasses import dataclass
-from typing import List, Union, Optional
+from typing import List
 from enum import Enum
 
-from .Payment import Paypal, CreditCard
+from .Payment import Payment
 from .Shipping import Shipping
-from .Product import Keyboard, Mouse, Switch, Keycap 
+from .Product import Product
+
 
 class OrderStatus(Enum):
-  OPEN = 0
-  CLOSE = 1
+    OPEN = 0
+    CLOSE = 1
 
-@dataclass
+
 class OrderItem:
-  product: Union[Keyboard,Mouse,Switch,Keycap]
-  qty: int
-  price: Optional[float] = 0
+    def __init__(self, product: Product, qty: int, price: float):
+        self.__product = product
+        self.__qty = qty
+        self.__price = price
 
-@dataclass
+    @property
+    def product(self):
+        return self.__product
+
+    @property
+    def qty(self):
+        return self.__qty
+
+    @property
+    def price(self):
+        return self.__price
+
+
 class Order:
-  items: List[OrderItem]
-  status: OrderStatus
-  shipping: Shipping
-  payment: Union[Paypal,CreditCard]
-  email: str
+    def __init__(
+        self,
+        email: str,
+        items: List[OrderItem],
+        shipping: Shipping,
+        payment: Payment,
+        status: OrderStatus,
+    ):
+        self.__email = email
+        self.__items = items
+        self.__shipping = shipping
+        self.__payment = payment
+        self.__status = status
 
-  def is_history_of(self, email):
-    if self.email == email:
-      return True
-  
-  # add prcess payment
-  def process_payment(self, payment_info):
-    if self.payment.pay(payment_info):
-      self.status = OrderStatus.CLOSE
-      return True
-    return False
+    def is_history_of(self, email):
+        if self.__email == email:
+            return True
+
+    def process_payment(self, payment_info):
+        if self.__payment.pay(payment_info):
+            self.__status = OrderStatus.CLOSE
+            return True
+        return False
+
+    def get_order_detail(self):
+        return {
+            "email": self.__email,
+            "items": self.__items,
+            "shipping": self.__shipping,
+            "payment": self.__payment,
+            "status": self.__status,
+        }
