74bdfc9209253826cf043b120e4ca76f5c43d522
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-26 17:01:13 +0700

    finish sign-in/sign-up

--- controllers/customer_ctrl.py
+++ controllers/customer_ctrl.py
@@ -1,34 +1,49 @@
 from fastapi import APIRouter, HTTPException
 from init_system import system
-from schemas.customer_shcema import SignIn, AddCartItem, GetCart 
+from schemas.customer_shcema import SignIn, SignUp, AddCartItem, GetCart
+from models.User import Customer
+from models.Cart import Cart
 
 router = APIRouter(prefix="/customer")
 
-@router.get("/get_all")
-async def get_all_customer():
-    return { "data": system.customers }
 
 @router.post("/sign_in")
 async def customer_login(body: SignIn):
     customer = system.sign_in(body.email, body.password)
     if not customer:
         raise HTTPException(status_code=400, detail="Wrong email or password")
-    return { "message": "Sign in successful", "data": customer }
+    return {"message": "Successfully Sign-in", "data": customer.email}
+
+
+@router.post("/sign_up")
+async def customer_register(body: SignUp):
+    email = body.email
+    password = body.password
+    firstname = body.firstname
+    lastname = body.lastname
+
+    if system.get_customer_by_email(email):
+        raise HTTPException(status_code=400, detail="This email already exist")
+
+    system.add_customer(Customer(email, password, firstname, lastname, Cart()))
+    return {"message": "Successfully Sign-up"}
+
 
 @router.post("/add_cart")
 async def add_cart_item(body: AddCartItem):
     email = body.email
     item_id = body.cart_item.product_id
     qty = body.cart_item.qty
-    
+
     customer = system.find_customer_by_email(email)
     product = system.find_product_by_id(item_id)
 
     if customer and product:
         customer.add_cart_item(product, qty)
-        return { "message": "success" }
+        return {"message": "success"}
     else:
         raise HTTPException(status_code=400, detail="Something went wrong")
+
 
 @router.get("/viewcart")
 async def view_cart(body: GetCart):
