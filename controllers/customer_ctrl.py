95b3a9888f5822fa6252f7c13517d230cabd89fb
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-28 23:26:31 +0700

    add try except to sign-in/sign-up

--- controllers/customer_ctrl.py
+++ controllers/customer_ctrl.py
@@ -1,32 +1,42 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
 from init_system import system
 from schemas.customer_shcema import SignIn, SignUp, AddCartItem, GetCart
 from models.User import Customer
 from models.Cart import Cart
+
+from models.CodeDiscount import *
 
 router = APIRouter(prefix="/customer")
 
 
 @router.post("/sign_in")
 async def customer_login(body: SignIn):
-    customer = system.sign_in(body.email, body.password)
-    if not customer:
-        raise HTTPException(status_code=400, detail="Wrong email or password")
-    return {"message": "Successfully Sign-in", "data": customer.email}
+    try:
+        customer = system.sign_in(body.email, body.password)
+        if not customer:
+            return JSONResponse(status_code=400, content="Wrong email or password.")
+        return {"detail": "Successfully Sign-in", "data": customer.email}
+    except Exception as e:
+        return JSONResponse(status_code=500, content=str(e))
 
 
 @router.post("/sign_up")
 async def customer_register(body: SignUp):
-    email = body.email
-    password = body.password
-    firstname = body.firstname
-    lastname = body.lastname
+    try:
+        email = body.email
+        password = body.password
+        firstname = body.firstname
+        lastname = body.lastname
 
-    if system.get_customer_by_email(email):
-        raise HTTPException(status_code=400, detail="This email already exist")
+        if system.get_customer_by_email(email):
+            raise HTTPException(status_code=400, detail="This email already exist")
 
-    system.add_customer(Customer(email, password, firstname, lastname, Cart()))
-    return {"message": "Successfully Sign-up"}
+        system.add_customer(Customer(email, password, firstname, lastname, Cart()))
+        return {"detail": "Successfully Sign-up"}
+
+    except Exception as e:
+        return JSONResponse(status_code=500, content=str(e))
 
 
 @router.post("/add_cart")
