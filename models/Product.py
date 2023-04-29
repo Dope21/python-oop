4acf5eb1027e132ae151a46feae92ad8ebdb1641
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-29 22:09:17 +0700

    remove dataclass

--- models/Product.py
+++ models/Product.py
@@ -1,112 +1,77 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class Product:
-    id: str
-    name: str
-    price: float
-    description: str
-    image: str
-    qty: int
+    def __init__(self, id, name, price, description, image, qty, last_update):
+        self.__id = id
+        self.__name = name
+        self.__price = price
+        self.__description = description
+        self.__image = image
+        self.__qty = qty
 
 
 #############################################################   Keyboard inherite form product #########################################################
-@dataclass
+
+
 class Keyboard(Product):
-    version: str
-    type: str
-    switches: str
-    color: str
+    def __init__(
+        self,
+        id,
+        name,
+        price,
+        description,
+        image,
+        qty,
+        version,
+        type,
+        switches,
+        color,
+    ):
+        super().__init__(id, name, price, description, image, qty)
+        self.__version = version
+        self.__type = type
+        self.__switches = switches
+        self.__color = color
 
 
 ############################################################# Mouse ###########################################################
-@dataclass
+
+
 class Mouse(Product):
-    connection: str
-    color: str
+    def __init__(
+        self, id, name, price, description, image, qty, last_update, connection, color
+    ):
+        super().__init__(id, name, price, description, image, qty, last_update)
+        self.__connection = connection
+        self.__color = color
 
 
 ########################################################### Switch ########################################
-@dataclass
+
+
 class Switch(Product):
-    option: str
-    quantity_set: int
-
-    ############### id
-    @property
-    def id(self) -> str:
-        return self.id
-
-    @id.setter
-    def id(self, id: str):
-        self.id
-
-    ################# name
-    @property
-    def name(self) -> str:
-        return self.name
-
-    @name.setter
-    def name(self, name: str):
-        self.name
-
-    ################# price
-    @property
-    def price(self) -> float:
-        return self.price
-
-    @price.setter
-    def price(self, price: float):
-        self.price
-
-    ######################### description
-    @property
-    def description(self) -> str:
-        return self.description
-
-    @description.setter
-    def description(self, description: str):
-        self.description
-
-    ############################## image
-    @property
-    def image(self) -> str:
-        return self.image
-
-    @image.setter
-    def image(self, image: str):
-        self.image
-
-    ###################### qty
-    @property
-    def qty(self) -> str:
-        return self.qty
-
-    @qty.setter
-    def qty(self, qty: str):
-        self.qty
-
-    ###################### connection
-    @property
-    def option(self) -> str:
-        return self.option
-
-    @option.setter
-    def option(self, option: str):
-        self.option
-
-    ############################ color
-    @property
-    def quantity_set(self) -> str:
-        return self.quantity_set
-
-    @quantity_set.setter
-    def quantity_set(self, quantity_set: str):
-        self.quantity_set
+    def __init__(
+        self,
+        id,
+        name,
+        price,
+        description,
+        image,
+        qty,
+        last_update,
+        option,
+        quantity_set,
+    ):
+        super().__init__(id, name, price, description, image, qty, last_update)
+        self.__option = option
+        self.__quantity_set = quantity_set
 
 
-# adding keycap
-@dataclass
+########################################################### Keycap ########################################
+
+
 class Keycap(Product):
-    pass
+    def __init__(
+        self, id, name, price, description, image, qty, last_update, version, color
+    ):
+        super().__init__(id, name, price, description, image, qty, last_update)
+        self.__version = version
+        self.__color = color
