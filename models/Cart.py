053362de3f3e4f5af2e50232df228fd85b190d9b
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-04-29 22:05:18 +0700

    remove dataclass

--- models/Cart.py
+++ models/Cart.py
@@ -1,25 +1,18 @@
-from dataclasses import dataclass, field
-from typing import List, Union
-
-from .Product import Keyboard, Switch, Keycap, Mouse
+from .Product import *
 
 
-@dataclass
 class CartItem:
-    product: Union[Keyboard, Keycap, Switch, Mouse]
-    qty: int
+    def __init__(self, product: Product, qty: int):
+        self.__product = product
+        self.__qty = qty
 
-    def get_product(self):
-        return self.product
+    @property
+    def product(self):
+        return self.__product
 
-    def set_product(self, product):
-        self.product = product
-
-    def get_quantity(self):
-        return self.quantity
-
-    def set_quantity(self, qty):
-        self.qty = qty
+    @property
+    def qty(self):
+        return self.__qty
 
 
 class Cart:
@@ -30,9 +23,8 @@ class Cart:
         if isinstance(item, CartItem):
             self.cart_items.append(item)
             return True
-        else:
-            return False
+        raise Exception("Please add CartItem object.")
 
     def remove_item(self, item):
-        if item in self.cart_items:
-            self.cart_items.remove(item)
+        if item in self.__cart_items:
+            self.__cart_items.remove(item)
