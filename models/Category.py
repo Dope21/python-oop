d80c987ad077d529ad763bab4e446ad06b7e07d1
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-05-01 00:01:21 +0700

    create create_product method

--- models/Category.py
+++ models/Category.py
@@ -1,21 +1,73 @@
-from dataclasses import dataclass
-from typing import List
+from .Product import *
 
-from .Product import Product
 
-@dataclass
 class Category:
-    name: str
-    products: List[Product]
+    def __init__(self, name):
+        self.__name = name
+        self.__product = []
 
-    def get_product_by_id(self, id):
-        for product in self.products:
-            if id == product.id:
-                return product
-        return False
+    @property
+    def name(self):
+        return self.__name
 
-    def get_product_by_name(self, name):
-        for product in self.products:
-            if name == product.name:
-                return product
-     
+    def create_product(self, **kwargs):
+        data = kwargs
+        if len(self.__product) != 0:
+            pro_id = self.__product[-1].id + 1
+        else:
+            pro_id = 1
+
+        if data.kind == "keyboard":
+            product = Keyboard(
+                pro_id,
+                data.name,
+                data.price,
+                data.description,
+                data.image,
+                data.qty,
+                data.version,
+                data.type,
+                data.switches,
+                data.color,
+            )
+        elif data.kind == "keycap":
+            product = Keycap(
+                pro_id,
+                data.name,
+                data.price,
+                data.description,
+                data.image,
+                data.qty,
+                data.version,
+                data.color,
+            )
+        elif data.kind == "switch":
+            product = Switch(
+                pro_id,
+                data.name,
+                data.price,
+                data.description,
+                data.image,
+                data.qty,
+                data.option,
+                data.quantity_set,
+            )
+        else:
+            product = Mouse(
+                pro_id,
+                data.name,
+                data.price,
+                data.description,
+                data.image,
+                data.qty,
+                data.connection,
+                data.color,
+            )
+        self.add_product(product)
+        return product
+
+    def add_product(self, product):
+        if isinstance(product, Product):
+            self.__product.append(product)
+        else:
+            raise ValueError("Please add Product object.")
