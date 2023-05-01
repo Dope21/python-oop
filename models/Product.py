d70d2fe7a1655bb0243914d46ee75f2a170723e1
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-05-01 18:55:55 +0700

    add get_detail

--- models/Product.py
+++ models/Product.py
@@ -7,6 +7,10 @@ class Product:
         self.__image = image
         self.__qty = qty
 
+    @property
+    def id(self):
+        return self.__id
+
 
 #############################################################   Keyboard inherite form product #########################################################
 
@@ -31,6 +35,20 @@ class Keyboard(Product):
         self.__switches = switches
         self.__color = color
 
+    def get_detail(self):
+        return {
+            "id": self._Product__id,
+            "name": self._Product__name,
+            "price": self._Product__price,
+            "description": self._Product__description,
+            "image": self._Product__image,
+            "qty": self._Product__qty,
+            "version": self.__version,
+            "type": self.__type,
+            "switches": self.__switches,
+            "color": self.__color,
+        }
+
 
 ############################################################# Mouse ###########################################################
 
@@ -40,6 +58,18 @@ class Mouse(Product):
         super().__init__(id, name, price, description, image, qty)
         self.__connection = connection
         self.__color = color
+
+    def get_detail(self):
+        return {
+            "id": self._Product__id,
+            "name": self._Product__name,
+            "price": self._Product__price,
+            "description": self._Product__description,
+            "image": self._Product__image,
+            "qty": self._Product__qty,
+            "connection": self.__connection,
+            "color": self.__color,
+        }
 
 
 ########################################################### Switch ########################################
@@ -61,6 +91,18 @@ class Switch(Product):
         self.__option = option
         self.__quantity_set = quantity_set
 
+    def get_detail(self):
+        return {
+            "id": self._Product__id,
+            "name": self._Product__name,
+            "price": self._Product__price,
+            "description": self._Product__description,
+            "image": self._Product__image,
+            "qty": self._Product__qty,
+            "option": self.__option,
+            "quantity_set": self.__quantity_set,
+        }
+
 
 ########################################################### Keycap ########################################
 
@@ -70,3 +112,15 @@ class Keycap(Product):
         super().__init__(id, name, price, description, image, qty)
         self.__version = version
         self.__color = color
+
+    def get_detail(self):
+        return {
+            "id": self._Product__id,
+            "name": self._Product__name,
+            "price": self._Product__price,
+            "description": self._Product__description,
+            "image": self._Product__image,
+            "qty": self._Product__qty,
+            "version": self.__version,
+            "color": self.__color,
+        }
