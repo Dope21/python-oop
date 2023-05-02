36de84b4006fa45a404fd87249d0e7feb941dabf
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-05-02 22:03:29 +0700

    change change shorthand to normal

--- controllers/product_ctrl.py
+++ controllers/product_ctrl.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from init_system import system
-from schemas.product_shcema import CateName
+from schemas.product_shcema import CateName, FindById
 
 router = APIRouter(prefix="/product")
 
@@ -11,7 +11,10 @@ async def get_product_in_category(body: CateName):
         cate_name = body.cate_name
         category = system.get_category_by_name(cate_name)
 
-        product_list = [p.get_detail() for p in category.products]
+        product_list = []
+        for product in category.products:
+            product_list.append(product.get_detail())
+            product_list[-1]["category"] = cate_name
         return {"data": product_list}
 
     except ValueError as e:
@@ -20,14 +23,33 @@ async def get_product_in_category(body: CateName):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/get_all_category")
-async def get_all_category():
+@router.get("/get_all_product")
+async def get_all_product():
     try:
-        return {
-            "data": [
-                p.get_detail() for cate in system.categories for p in cate.products
-            ]
-        }
+        product_list = []
+        for cate in system.categories:
+            for p in cate.products:
+                product_list.append(p.get_detail())
+                product_list[-1]["category"] = cate.name
+        return {"data": product_list}
+
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/detail")
+async def get_product_detail(body: FindById):
+    try:
+        pro_id = int(body.pro_id)
+        cate_name = body.cate_name
+
+        category = system.get_category_by_name(cate_name)
+        product = category.get_product_by_id(pro_id)
+
+        return {"data": product.get_detail()}
+
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
