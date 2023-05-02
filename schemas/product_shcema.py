e1d30ef1252da6fbbbced383635a1bee33626dbb
Thanasak Limsila <86032673+Dope21@users.noreply.github.com>
2023-05-02 22:04:19 +0700

    schema for find product

--- schemas/product_shcema.py
+++ schemas/product_shcema.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
 
-class CateName(BaseModel):
-    cate_name : str
 
-class ProId(BaseModel):
-    id: str
-    
+class CateName(BaseModel):
+    cate_name: str
+
+
+class FindById(BaseModel):
+    pro_id: str
+    cate_name: str
+
+
 class ProName(BaseModel):
     name: str
-    
-
-    
